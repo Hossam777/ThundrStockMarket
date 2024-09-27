cefb2dd8e59915f234ca1f4396c6596352647a63
Hossam777 <31036061+Hossam777@users.noreply.github.com>
2024-09-27 14:19:21 +0300

    fix ios insests and splash screen view.

--- App.tsx
+++ App.tsx
@@ -1,28 +1,23 @@
 import React from 'react';
-import {StatusBar, useColorScheme} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
 import ExploreScreen from './src/screen/explorescreen/ExploreScreen';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import StaticSafeAreaInsets from 'react-native-static-safe-area-insets';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
   const onHideSplashScreen = () => {
     SplashScreen.hide();
   };
 
   return (
-    <SafeAreaView edges={['bottom', 'top']} style={{flex: 1}}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
+    <SafeAreaView
+      style={{
+        flex: 1,
+        paddingTop: StaticSafeAreaInsets.safeAreaInsetsTop,
+        paddingBottom: StaticSafeAreaInsets.safeAreaInsetsBottom,
+      }}>
       <ExploreScreen onHideSplashScreen={onHideSplashScreen} />
     </SafeAreaView>
   );
--- ios/ThundrStockMarket/AppDelegate.mm
+++ ios/ThundrStockMarket/AppDelegate.mm
@@ -7,12 +7,18 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"ThundrStockMarket";
-  // You can add your custom initial props in the dictionary below.
-  // They will be passed down to the ViewController used by React Native.
-  self.initialProps = @{};
-  [RNSplashScreen show];
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+    RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+    RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
+                                                     moduleName:@"ThundrStockMarket"
+                                              initialProperties:nil];
+    rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    UIViewController *rootViewController = [UIViewController new];
+    rootViewController.view = rootView;
+    self.window.rootViewController = rootViewController;
+    [self.window makeKeyAndVisible];
+    [RNSplashScreen show];
+    return YES;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
